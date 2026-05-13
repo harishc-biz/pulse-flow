99241ba1b3bee72367396a6c2298fef51e61bda2
CHHARIS <thoughtworks.chandrappa@extaccount.com>
2026-05-13 15:51:40 +0530

    Prod env: fixing the terraform issues

--- pulse-infra/terraform/key_vault.tf
+++ pulse-infra/terraform/key_vault.tf
@@ -2,6 +2,8 @@ locals {
   keyvault_name = lower(format("%spulseFlowKV", var.env))
 }
 
+data "azurerm_client_config" "current" {}
+
 module "keyvault" {
   source  = "Azure/avm-res-keyvault-vault/azurerm"
   version = "0.9.1"
